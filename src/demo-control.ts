7b55bae07a7dfcb68239e6a25fe2209e7567bf19
Yuanming Chen <yuanming@hotmail.com>
2021-11-11 22:10:18 -0700

    Fix code formatting

--- src/demo-control.ts
+++ src/demo-control.ts
@@ -1,7 +1,7 @@
 import { FASTElement, customElement, attr, html } from '@microsoft/fast-element';
 
 const template = html<DemoControl>`
-<fast-flipper direction="previous" ?disabled=${x => x.firstPage == false} @click=$${(x)=>x.pageNumber++}>
+<fast-flipper direction="previous" ?disabled=${x => x.firstPage == false} @click=${(x)=>x.pageNumber++}>
 </fast-flipper>
 <p>Page number=${(x)=>x.pageNumber}</p>
 `;
